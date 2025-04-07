8ece926ab8b99df22c24a47c105d380890f47b1e
Lucas Pereira <lucasmbp07@gmail.com>
2025-04-07 14:22:36 -0300

    margens e botões

--- doc.md
+++
@@ -1,2 +0,0 @@
-# Minha documentação
-
--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 function togglemode() {
   const html = document.documentElement
-  
-  //if(html.classList.contains('light')){
-  //  html.classList.remove('light')
-  //}
-  //else{
-  //  html.classList.add('light')
-  //}
   html.classList.toggle('light')
+
+  // pegar tag img
+  const img = document.querySelector("#profile img")
+
+  // substituir a imagem
+  if(html.classList.contains('light')) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute('src', './assets/avatar-light.png')
+  } else {
+    // se tiver sem light mode, imagem normal
+    img.setAttribute('src', './assets/avatar.png')
+  }
 }
